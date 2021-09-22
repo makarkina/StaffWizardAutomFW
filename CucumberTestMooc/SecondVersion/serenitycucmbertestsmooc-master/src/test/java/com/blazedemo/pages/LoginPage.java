6c63585f3d2529f261cd48ad1eb263b61269841a
makarkina <anna.makarkina@gmail.com>
2021-09-22 10:42:30 -0400

    Delete CucumberTestMooc/SecondVersion/serenitycucmbertestsmooc-master directory

--- CucumberTestMooc/SecondVersion/serenitycucmbertestsmooc-master/src/test/java/com/blazedemo/pages/LoginPage.java
+++
@@ -1,8 +0,0 @@
-package com.blazedemo.pages;
-
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("http://vgroll.com:8080/vroll/")
-public class LoginPage extends PageObject {
-}
